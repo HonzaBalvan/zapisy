d50cdf7962a9f968679c0fd72c8d8f6af3507891
HonzaBalvan <romanovsky.honza@gmail.com>
2025-11-21 13:19:44 +0100

    lingebra dú 7

---
+++ CUNI/ZS 2025/prog/08/dict_inversion.py
@@ -0,0 +1,10 @@
+#i = 0
+#input_list = []
+
+#while str(input()) != "---":
+#    input_list.append(str(input()).split(":"))
+
+#print(input_list)
+#print(type(input_list))
+input_list = [str(x) for x in input().split()]
+print(input_list)
