f3b08db7026e2a1ff69d3257d03010164d8da204
HonzaBalvan <romanovsky.honza@gmail.com>
2025-12-08 16:22:45 +0100

    prog 10 2

--- CUNI/ZS 2025/prog/10/element_position.py
+++ CUNI/ZS 2025/prog/10/element_position.py
@@ -1,27 +1,33 @@
 len_a, len_b = [int(x) for x in input().split()]
 
 list_a = [int(x) for x in input().split()]
-
 list_b = [int(x) for x in input().split()]
 
 list_c = list_a + list_b
 
 list_c.sort()
 
-print(list_c)
-
 indexes_a = []
 indexes_b = []
 
-a = list_a[0]
 iterator_a = iter(list_a)
+a = next(iterator_a)
 
 for l_bound in range(len_a + len_b):
-    if a == list_c[l_bound]:
-        indexes_a += [l_bound]
+    if a == list_a[-1]:
+        if a == list_c[l_bound]:
+            indexes_a += [l_bound+1]
+        else:
+            indexes_b += [l_bound+1]
+    elif a == list_c[l_bound]:
+        indexes_a += [l_bound+1]
         a = next(iterator_a)
     else:
-        indexes_b += [l_bound]
+        indexes_b += [l_bound+1]
 
-print(indexes_a)
-print(indexes_b)
+for i in indexes_a:
+    print(i, end = " ")
+print()
+for i in indexes_b:
+    print(i, end = " ")
+print()
